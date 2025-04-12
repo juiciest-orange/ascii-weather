dae964c5e5b58de18e8623e73ae50e4bb777e0aa
Carena Wong <carenawong92@gmail.com>
2025-04-12 10:57:55 -0700

    fixed quotation marks and cleanup iimage files after each script run

--- weather_to_ascii.py
+++ weather_to_ascii.py
@@ -4,8 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from datetime import datetime as dt
-import os
 import cv2
+import os
 
 print('[%s] Python Radar Weather Image to ASCII Conversion' %dt.now().strftime("%Y-%m-%d %H:%M:%S"))
 
@@ -13,20 +13,19 @@ print('[%s] Python Radar Weather Image to ASCII Conversion' %dt.now().strftime("
 # http://docs.seleniumhq.org/download/
 # or a direct url:
 # http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar
-os.environ["SELENIUM_SERVER_JAR"] = "selenium-server-standalone-2.41.0.jar"
+# os.environ["SELENIUM_SERVER_JAR"] = "selenium-server-standalone-2.41.0.jar"
 # note: I've put this jar file in the same folder as this python file
 
 # Start Webdriver for Chrome
-cService = webdriver.ChromeService('/usr/bin/chromedriver') # For Chromium
+cService = webdriver.ChromeService('/usr/bin/chromedriver')
 browser = webdriver.Chrome(service = cService)
 url = "https://www.weatherbug.com/weather-forecast/now/lake-tahoe-ca-95728"
 browser.get(url)
 path = os.getcwd()+'/scrape.png'
-
-# Wait a little bit and take screenshot
 browser.implicitly_wait(5)
 el = browser.find_element(By.TAG_NAME, 'map-widget')
 el.screenshot(path)
+# browser.save_screenshot(path)
 browser.quit()
 
 # Crop the scraped image
@@ -38,10 +37,12 @@ x_start, x_end = 150, 350
 # Crop the image
 cropped_image = image[y_start:y_end, x_start:x_end]
 # Save the cropped image
-cv2.imwrite(os.getcwd()+"/"+"cropped_image.png", cropped_image)
+cv2.imwrite(os.getcwd()+'/cropped_image.png', cropped_image)
 
-
-my_art = AsciiArt.from_image(os.getcwd()+"/cropped_image.png")
-my_art.image = ImageEnhance.Brightness(my_art.image).enhance(0.2)
+my_art = AsciiArt.from_image(os.getcwd()+'/cropped_image.png')
 my_art.to_html_file('/var/www/html/ascii_art.html', columns=200, width_ratio=2,
-	additional_styles='font-size: 1;')
+                   additional_styles='font-size: 1;')
+
+# Cleanup files
+os.remove(os.getcwd()+'/cropped_image.png')
+os.remove(os.getcwd()+'/scrape.png')
